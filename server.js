e2ba91ef743a1471f8a99cfa2eb3c6b1548ae549
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-11 18:58:30 -0700

    server is up and running port is connected and express is set up

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const server = express();
+server.use(cors());
+server.use(express.json());
 
 mongoose
   .connect(`mongodb://jake:1test1@ds131954.mlab.com:31954/dream-trekking`)
   .then(() => console.log(`\n====  connected to mongo ====\n`))
   .catch(() => console.log(`error connecting to mongo`));
+
   server.get("/", (req, res) => {
     res.status(200).json({ api: `the api is running on port ${port}` });
   });
