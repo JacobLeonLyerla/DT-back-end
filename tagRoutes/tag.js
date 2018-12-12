dd6f0122e3471bc57bef0f4d5510eb6f6652a985
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-11 21:25:00 -0700

    more model fields added

--- tagRoutes/tag.js
+++ tagRoutes/tag.js
@@ -1,19 +1,27 @@
 const mongoose = require("mongoose");
 
 const Tag = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-      },
-      tag: {
-        type: Array,
-        required: true,
-            },
-    latStart:Number,
-    lngStart:Number,
-    latEnd:Number,
-    lngEnd:Number,
-    zoom:Number,
+  name: {
+    type: String,
+    required: true
+  },
+  country: String,
+  region: String,
+  city: String,
+  language: Array,
+  tag: {
+    type: Array,
+    required: true
+  },
+  latStart: Number,
+  lngStart: Number,
+  latEnd: Number,
+  lngEnd: Number,
+  zoom: Number,
+  createdOn:{
+    type:Date,
+    default: Date.now,
+},
 });
 
 module.exports = mongoose.model("Tag", Tag);
