bceb3a1d6f7d9d99b41720517999172333ad5079
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-11 21:25:23 -0700

    set up router and tested post route

--- tagRoutes/tagRouter.js
+++ tagRoutes/tagRouter.js
@@ -1,9 +1,50 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Tags = require('./tag')
+const Tag = require("./tag");
 
-
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Tag.findById(id)
+    .then(response => {
+      res.status(202).json(response);
+    })
+    .catch(err => {
+      res.status(500).json;
+    });
+});
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+  const options = {
+    new: true
+  };
+  Tag.findByIdAndUpdate(id, update, options)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
 router.get("/", (req, res) => {
-    res.status(200).json({ api: "Tags router tested" });
-  });
+  Tag.find()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/", (req, res) => {
+  const tag = new Tag(req.body);
+  tag
+    .save()
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
 module.exports = router;
