3add60244b297a63e35a25fd066ee8f6d070756a
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-16 02:24:49 -0700

    set up jwt  folder and built out code

---
+++ jwt/jwt.js
@@ -0,0 +1,26 @@
+const {ExtractJwt} = require("passport-jwt");
+const JwtStrategy  =  require("passport-jwt").Strategy;
+const passport = require("passport");
+const User = require("../tagRoutes/user");
+const jwtOptions ={
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.secretket
+};
+const jwtStrategy = new JwtStrategy(jwtOptions,(payload,done)=>{
+
+    User.findById(payload.sub)
+    .select("-password")
+    .then(user=>{
+        if(user){
+            done(null,user);
+        }else{
+            done(null,false);
+        }
+        })
+        .catch(err=>{
+            done(err,false)
+        })
+    })
+})
+const protected = passport.authenticate("jwt",{session:false});
+module.exports ={jwtStrategy,protected};
