e28399d19ae971b541a431838148a7b6285665a9
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-16 03:12:46 -0700

    set up a basic auth router

---
+++ routers/authRouter.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { login, authenticate, localStrategy } = require("../controllers/login");
+const { protected, jwtStrategy } = require("../jwt/jwt");
+const passport = require("passport");
+const createUser = require("../controllers/createUser");
+
+passport.use(jwtStrategy);
+passport.use(localStrategy);
+
+router.route("/register").post(createUser);
+router.route("/login").post(authenticate, login);
+
+module.exports = router;
