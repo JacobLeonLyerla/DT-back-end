9122eaac21e3913bb43cb3e9838874a341ca7db9
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-16 02:41:05 -0700

    more validation checks for the register

--- validation/register.js
+++ validation/register.js
@@ -1,21 +1,32 @@
 const Validator = require("validator");
 const isEmpty = require("./is-empty");
 
-const validateRegisterInput =({username,email,password,password2})=>{
-    let errors = {};
-    username = !isEmpty(username)?username:"";
-    email =!isEmpty(email)?email:"";
-    password = !isEmpty(password)?password:"";
-    password2 = !isEmpty(password2)?password2:"";
-if(!validator.isLength(username,{min:6,max:30})){
-    errors.username = "Username most be six or more characters, no more than thirty."
-}
-if(validator.isEmpty(username)){
-    errors.username ="Please enter a username."
-}
-if(validator.isEmpty(email)){
-    errors.username ="Please enter an Email."
-}
+const validateRegisterInput = ({ username, email, password, password2 }) => {
+  let errors = {};
+  username = !isEmpty(username) ? username : "";
+  email = !isEmpty(email) ? email : "";
+  password = !isEmpty(password) ? password : "";
+  password2 = !isEmpty(password2) ? password2 : "";
+  if (!Validator.isLength(username, { min: 6, max: 30 })) {
+    errors.username =
+      "Username most be six or more characters, no more than thirty.";
+  }
+  if (Validator.isEmpty(username)) {
+    errors.username = "Please enter a username.";
+  }
+  if (Validator.isEmpty(email)) {
+    errors.email = "Please enter an Email.";
+  }
+  if (!Validator.isEmail(email)) {
+    errors.email = "This email is not valid, please try another.";
+  }
+  if (Validator.isEmpty(password)) {
+    errors.password = "Please enter a password.";
+  }
 
-}
-module.exports = validateRegisterInput
+  if (!Validator.isLength(password, { min: 6, max: 30 })) {
+    errors.password =
+      "Password most be six or more characters, no more than thirty.";
+  }
+};
+module.exports = validateRegisterInput;
