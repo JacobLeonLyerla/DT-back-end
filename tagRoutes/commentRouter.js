88b11ce564dd5ef76af514626578ad6589a7597b
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-21 02:15:00 -0700

    built a comment router

---
+++ tagRoutes/commentRouter.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const { protected, jwtStrategy } = require("../jwt/jwt");
+
+const Comment = require("./Comment");
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Comment.findById(id)
+    .then(response => {
+      res.status(202).json(response);
+    })
+    .catch(err => {
+      res.status(500).json;
+    });
+});
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+  const options = {
+    new: true
+  };
+  Comment.findByIdAndUpdate(id, update, options)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+router.get("/",protected, (req, res) => {
+  Comment.find()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/", (req, res) => {
+  const comment = new Comment(req.body);
+  comment
+    .save()
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+module.exports = router;
