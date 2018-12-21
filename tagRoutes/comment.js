4a28198661da8db8b3e95a4def95f0505bfd561c
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-21 02:14:52 -0700

    built a comment model

---
+++ tagRoutes/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+const Comment = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true
+  },
+  replyTo:{
+      type:ObjectId,
+      ref:"User"
+  },
+  user:{
+      type:String,
+      required:true
+  },
+  createdOn:{
+    type:Date,
+    default: Date.now,
+},
+});
+
+module.exports = mongoose.model("Comment", Comment);
