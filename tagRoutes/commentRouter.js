08bd22162dab40b4c8f67855776d721287f67d32
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 17:51:59 -0700

    another test

--- tagRoutes/commentRouter.js
+++ tagRoutes/commentRouter.js
@@ -7,7 +7,7 @@ router.get("/:id", (req, res) => {
   const { id } = req.params;
   Comment.findById(id).populate({ 
     path : 'replies',
-    populate : { path : 'replies'}
+    populate : { path : 'replies',path:"replyTo"}
 })
     .then(response => {
       res.status(202).json(response);
