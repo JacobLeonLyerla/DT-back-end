8c49edc043eb47bf45756124ffc5bc5b8b8e93a4
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 17:27:52 -0700

    still trying to figure out the nesting issue, i have to keep commiting because i need to update heroku

--- tagRoutes/commentRouter.js
+++ tagRoutes/commentRouter.js
@@ -5,7 +5,7 @@ const Comment = require("./comment");
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  Comment.findById(id).populate('replies').populate('replies.replyTo')
+  Comment.findById(id).populate('replies').populate('replies.replies')
     .then(response => {
       res.status(202).json(response);
     })
