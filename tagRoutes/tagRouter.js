a3443ecf689b7108820fc6f7216f79e76823f299
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-03 22:29:13 -0700

    trying to sort my tags

--- tagRoutes/tagRouter.js
+++ tagRoutes/tagRouter.js
@@ -28,7 +28,7 @@ router.put("/:id", (req, res) => {
     });
 });
 router.get("/",protected, (req, res) => {
-  Tag.find().populate('comments')
+  Tag.find().sort('name').populate('comments')
     .then(response => {
       res.status(200).json(response);
     })
