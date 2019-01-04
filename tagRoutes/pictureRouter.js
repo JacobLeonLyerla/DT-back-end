be5bfcb6d331c39b7bf062909dccc8a8ad2c9caf
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-03 22:50:21 -0700

    that did not work i am going to try this.

--- tagRoutes/pictureRouter.js
+++ tagRoutes/pictureRouter.js
@@ -27,7 +27,7 @@ router.put("/:id", (req, res) => {
     });
 });
 router.get("/",protected, (req, res) => {
-  Picture.find().sort('priorty', 'name')
+  Picture.find().sort('priorty' ).sort('name')
     .then(response => {
       res.status(200).json(response);
     })
