3637e781e5a3856c0078bfd37fe1a45416db1520
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-13 22:55:58 -0700

    for matting the  jwt file

--- jwt/jwt.js
+++ jwt/jwt.js
@@ -2,26 +2,23 @@ const { ExtractJwt } = require("passport-jwt");
 const JwtStrategy = require("passport-jwt").Strategy;
 const passport = require("passport");
 const User = require("../tagRoutes/user");
-const jwtOptions ={
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: "secret212312"
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "secret212312"
 };
 const jwtStrategy = new JwtStrategy(jwtOptions, (payload, done) => {
-    User.findById(payload.sub)
+  User.findById(payload.sub)
     .select("-password")
-    .then(user=>{
-        if(user){
-            done(null,user);
-        }else{
-            done(null,false);
-        }
-        })
-        .catch(err=>
-            done(err,false)
-        )
-    });
+    .then(user => {
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    })
+    .catch(err => done(err, false));
+});
 
-    const protected = passport.authenticate("jwt", { session: false });
+const protected = passport.authenticate("jwt", { session: false });
 
-    module.exports = { jwtStrategy, protected };
-    
+module.exports = { jwtStrategy, protected };
