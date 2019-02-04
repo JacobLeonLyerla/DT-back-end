17cd1355528c91b0e545df5eb2332552c4df04cc
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-03 23:45:55 -0700

    create user  formatted for better readability.

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,14 +1,17 @@
 const User = require("../tagRoutes/user");
+
 const validateRegisterInput = require("../validation/register");
 
 const createUser = (req, res) => {
   const myNewUser = new User(req.body);
 
   const { errors, isValid } = validateRegisterInput(req.body);
+
   if (!isValid) return res.status(400).json(errors);
 
   myNewUser
     .save()
+
     .then(user => {
       res.status(201).json(user);
     })
