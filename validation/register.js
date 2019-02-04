f6286d29480ad56f69c7bc8dd0c3b0475951d1a2
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-03 23:47:45 -0700

    register formatted for better readability.

--- validation/register.js
+++ validation/register.js
@@ -1,25 +1,36 @@
 const Validator = require("validator");
+
 const isEmpty = require("./is-empty");
 
 const validateRegisterInput = ({ username, email, password, password2 }) => {
+
   let errors = {};
+
   username = !isEmpty(username) ? username : "";
+
   email = !isEmpty(email) ? email : "";
+
   password = !isEmpty(password) ? password : "";
+
   password2 = !isEmpty(password2) ? password2 : "";
+
   if (!Validator.isLength(username, { min: 2, max: 30 })) {
     errors.username =
       "Username most be six or more characters, no more than thirty.";
   }
+
   if (Validator.isEmpty(username)) {
     errors.username = "Please enter a username.";
   }
+
   if (Validator.isEmpty(email)) {
     errors.email = "Please enter an Email.";
   }
+
   if (!Validator.isEmail(email)) {
     errors.email = "This email is not valid, please try another.";
   }
+
   if (Validator.isEmpty(password)) {
     errors.password = "Please enter a password.";
   }
@@ -28,6 +39,7 @@ const validateRegisterInput = ({ username, email, password, password2 }) => {
     errors.password =
       "Password most be six or more characters, no more than thirty.";
   }
+
   if (Validator.isEmpty(password2)) {
     errors.password2 = "Password must be confirmed.";
   }
@@ -38,7 +50,10 @@ const validateRegisterInput = ({ username, email, password, password2 }) => {
 
   return {
     errors,
+
     isValid: isEmpty(errors)
   };
+
 };
+
 module.exports = validateRegisterInput;
