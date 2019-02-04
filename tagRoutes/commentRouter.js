da8ce759642e51ea6eacac66fe06a48844c6ca82
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-03 23:46:35 -0700

    comment router formatted for better readability.

--- tagRoutes/commentRouter.js
+++ tagRoutes/commentRouter.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+
 const { protected, jwtStrategy } = require("../jwt/jwt");
 
 const Comment = require("./comment");
@@ -6,43 +7,56 @@ const Comment = require("./comment");
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   Comment.findById(id)
+
     .populate({
       path: "replies",
+
       populate: { path: "replies" }
     })
     .then(response => {
       res.status(202).json(response);
     })
-    .catch(err => {
+
+    .catch(() => {
       res.status(500).json;
     });
 });
+
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-  Comment.findByIdAndRemove(id).then(response => {
+
+  Comment.findByIdAndRemove(id).then(() => {
     res.status(204).end();
   });
 });
+
 router.put("/:id", (req, res) => {
   const { id } = req.params;
+
   const update = req.body;
+
   const options = {
     new: true
   };
+
   Comment.findByIdAndUpdate(id, update, options)
     .then(response => {
       res.status(200).json(response);
     })
+
     .catch(err => {
       res.status(500).json({ error: err });
     });
 });
+
 router.get("/", protected, (req, res) => {
   Comment.find()
     .populate("replies")
+
     .then(response => {
       res.status(200).json(response);
     })
+
     .catch(err => {
       res.status(500).json({ error: err });
     });
@@ -55,8 +69,10 @@ router.post("/", (req, res) => {
     .then(response => {
       res.status(201).json(response);
     })
+
     .catch(err => {
       res.status(500).json({ error: err });
     });
 });
+
 module.exports = router;
