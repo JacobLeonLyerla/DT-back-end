cc146915a26bcfec6ee9a16ffb39174941858551
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-03 23:46:49 -0700

    tag formatted for better readability.

--- tagRoutes/tag.js
+++ tagRoutes/tag.js
@@ -1,51 +1,82 @@
 const mongoose = require("mongoose");
+
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Tag = new mongoose.Schema({
   name: {
     type: String,
+
     required: true
   },
+
   user: {
     type: String,
+
     required: true
   },
+
   likes: String,
+
   dislikes: String,
+
   locationName: String,
+
   country: String,
+
   region: String,
+
   city: String,
+
   language: Array,
+
   tag: {
     type: Array,
+
     required: true
   },
+
   latStart: Number,
+
   lngStart: Number,
+
   latEnd: Number,
+
   lngEnd: Number,
+
   zoom: Number,
+
   description: String,
+
   comments: [
+
     {
       type: ObjectId,
+
       ref: "Comment"
     }
+
   ],
+
   unreadComment: {
     type: Number,
+
     default: 0
   },
+
   unreadLike: {
     type: Number,
+
     default: 0
   },
+
   markers: Array,
+
   createdOn: {
     type: Date,
+
     default: Date.now
   }
+  
 });
 
 module.exports = mongoose.model("Tag", Tag);
