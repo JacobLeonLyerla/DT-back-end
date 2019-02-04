ad11ea2cf260f9453a46697022f56cc31d9885d4
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-03 23:46:43 -0700

    picture formatted for better readability.

--- tagRoutes/picture.js
+++ tagRoutes/picture.js
@@ -3,19 +3,29 @@ const mongoose = require("mongoose");
 const Picture = new mongoose.Schema({
   name: {
     type: String,
+
     required: true
   },
+
   tag: {
     type: Array
   },
+
   country: String,
+
   region: String,
+
   city: String,
+
   lat: Number,
+
   lng: Number,
+
   priority: Number,
+
   createdOn: {
     type: Date,
+
     default: Date.now
   }
 });
