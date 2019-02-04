39565e971ffc7cb22694e5c215da05228964b1d0
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-03 23:46:27 -0700

    comment formatted for better readability.

--- tagRoutes/comment.js
+++ tagRoutes/comment.js
@@ -1,29 +1,39 @@
 const mongoose = require("mongoose");
+
 const ObjectId = mongoose.Schema.Types.ObjectId;
+
 const Comment = new mongoose.Schema({
   comment: {
     type: String,
+
     required: true
   },
+
   replyTo: [
     {
       type: ObjectId,
+
       ref: "Comment"
     }
   ],
+
   replies: [
     {
       type: ObjectId,
+
       ref: "Comment"
     }
   ],
 
   username: {
     type: String,
+
     required: true
   },
+
   createdOn: {
     type: Date,
+
     default: Date.now
   }
 });
