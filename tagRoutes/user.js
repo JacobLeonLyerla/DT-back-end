7c13d5ccc85a03e8b1db2943658075bf2f96ca00
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-03 23:47:08 -0700

    user formatted for better readability.

--- tagRoutes/user.js
+++ tagRoutes/user.js
@@ -1,53 +1,76 @@
 // place for user model
 const mongoose = require("mongoose");
+
 const uniqueValidator = require("mongoose-unique-validator");
+
 const bcrypt = require("bcrypt");
+
 const SALT_ROUNDS = 11;
+
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 //User Model Schema
 const userSchema = mongoose.Schema({
   username: {
     type: String,
+
     required: true,
+
     unique: true,
+
     lowercase: true
   },
+
   email: {
     type: String,
+
     required: true,
+
     unique: true,
+
     lowercase: true
   },
+
   password: {
     type: String,
+
     required: true
   },
+
   post: [
     {
       type: ObjectId,
+
       ref: "Tag"
     }
   ],
   favorites: [
     {
       type: ObjectId,
+
       ref: "Tag"
     }
   ],
+
   comments: [
     {
       type: ObjectId,
+
       ref: "Comment"
     }
   ],
+
   notifications: Array,
+
   membership: {
     type: Boolean,
+
     default: false
   },
+
   subscription: String
 });
+
 userSchema.pre("save", function(next) {
   if (!this.isModified("password")) return next();
 
@@ -57,16 +80,20 @@ userSchema.pre("save", function(next) {
     return next();
   });
 });
+
 userSchema.methods.checkPassword = function(plainTextPW, callback) {
   return bcrypt.compare(plainTextPW, this.password, function(err, isValid) {
     if (err) {
       return callback(err);
     }
+
     callback(null, isValid);
   });
 };
+
 userSchema.methods.addLetter = function(letter_id) {
   this.letters.push(letter_id);
 };
+
 userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model("User", userSchema);
