bd0b1f958ee169402abde08f01f7bad3ad7b6def
Jacob <jacobleonlyerla@gmail.com>
2020-03-10 22:32:22 -0700

    formatting

--- validation/login.js
+++ validation/login.js
@@ -2,20 +2,29 @@ const Validator = require("validator");
 
 const isEmpty = require("../validation/is-empty");
 
-const validateLoginInput = ({ username, password }) => {
+const validateLoginInput = ({
+  username,
+  password
+}) => {
   let errros = {};
 
   username = !isEmpty(username) ? username : "";
 
   password = !isEmpty(password) ? password : "";
 
-  if (!Validator.isLength(username, { min: 2, max: 30 })) {
+  if (!Validator.isLength(username, {
+      min: 2,
+      max: 30
+    })) {
     errros.username = "Username must be between two and thirty characters.";
   }
   if (Validator.isEmpty(username)) {
     errros.username = "You must enter a Password.";
   }
-  if (!Validator.isLength(password, { min: 6, max: 30 })) {
+  if (!Validator.isLength(password, {
+      min: 6,
+      max: 30
+    })) {
     errros.password = "Password must be between two and thirty characters.";
   }
   return {
