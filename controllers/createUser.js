b0f7a589552638afc7f6239691c21058dde51fe3
Jacob <jacobleonlyerla@gmail.com>
2020-03-10 22:31:35 -0700

    formatting

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -5,7 +5,10 @@ const validateRegisterInput = require("../validation/register");
 const createUser = (req, res) => {
   const myNewUser = new User(req.body);
 
-  const { errors, isValid } = validateRegisterInput(req.body);
+  const {
+    errors,
+    isValid
+  } = validateRegisterInput(req.body);
 
   if (!isValid) return res.status(400).json(errors);
 
