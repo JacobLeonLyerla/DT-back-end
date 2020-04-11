10598c09fceb2c0803583476c4085fa25e6b1530
Jacob <jacobleonlyerla@gmail.com>
2020-04-10 22:15:34 -0700

    caracter user

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -5,20 +5,17 @@ const validateRegisterInput = require("../validation/register");
 const createUser = (req, res) => {
   const myNewUser = new User(req.body);
 
-  const {
-    errors,
-    isValid
-  } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body);
 
   if (!isValid) return res.status(400).json(errors);
 
   myNewUser
     .save()
 
-    .then(user => {
+    .then((user) => {
       res.status(201).json(user);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json(err);
     });
 };
