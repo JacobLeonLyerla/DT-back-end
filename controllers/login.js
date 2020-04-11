8ab2c9522a6435016b638edf3d02889c476ed7fb
Jacob <jacobleonlyerla@gmail.com>
2020-04-10 22:15:41 -0700

    login fixing

--- controllers/login.js
+++ controllers/login.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 
 const LocalStrategy = require("passport-local");
 
-const makeToken = user => {
+const makeToken = (user) => {
   const payload = {
     sub: user._id,
 
@@ -14,58 +14,56 @@ const makeToken = user => {
 
     email: user.email,
 
-    membership: user.membership
+    membership: user.membership,
   };
 
   const options = {
-    expiresIn: "2h"
+    expiresIn: "2h",
   };
 
   return jwt.sign(payload, process.env.key, options);
 };
 
 const localStrategy = new LocalStrategy((username, password, done) => {
-  User.findOne({
-    username
-  }, (err, user) => {
-    if (err) return done(err);
-    if (!user) return done(null, false);
-
-    user.checkPassword(password, (err, isMatch) => {
+  User.findOne(
+    {
+      username,
+    },
+    (err, user) => {
       if (err) return done(err);
-      if (isMatch) {
-        const {
-          _id,
-          username,
-          email,
-          membership
-        } = user;
-        return done(null, {
-          _id,
-          username,
-          email,
-          membership
-        });
-      }
+      if (!user) return done(null, false);
 
-      return done(null, false);
-    });
-  });
+      user.checkPassword(password, (err, isMatch) => {
+        if (err) return done(err);
+        if (isMatch) {
+          const { _id, username, email, membership } = user;
+          return done(null, {
+            _id,
+            username,
+            email,
+            membership,
+          });
+        }
+
+        return done(null, false);
+      });
+    }
+  );
 });
 
 const authenticate = passport.authenticate("local", {
-  session: false
+  session: false,
 });
 
 const login = (req, res) => {
   res.json({
     token: makeToken(req.user),
-    user: req.user
+    user: req.user,
   });
 };
 
 module.exports = {
   login,
   authenticate,
-  localStrategy
+  localStrategy,
 };
